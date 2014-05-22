f15ba2c4c978206a9e7a3e25de1c29eb9f481579
David Padilla <david@crowdint.com>
2014-05-22 13:40:08 -0500

    Translate the interfaces page

--- examples/interfaces/interfaces.sh
+++ examples/interfaces/interfaces.sh
@@ -6,5 +6,6 @@ $ go run interfaces.go
 78.53981633974483
 31.41592653589793
 
-# To learn more about Go's interfaces, check out this
-# [great blog post](http://jordanorelli.tumblr.com/post/32665860244/how-to-use-interfaces-in-go).
+# Para aprender más acerca de la manera en que
+# Go implementa las interfaces puedes leer este
+# [excelente blog post](http://jordanorelli.tumblr.com/post/32665860244/how-to-use-interfaces-in-go).
