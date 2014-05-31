0b246b4f709872cc171f8fb455fa3277e6b509ed
José Carlos Nieto <jose.carlos@menteslibres.net>
2014-05-31 11:34:22 -0500

    Translating: atomic-counters into contadores atómicos.

---
+++ examples/contadores-atomicos/contadores-atomicos.sh
@@ -0,0 +1,6 @@
+# Al ejecutar el programa podemos observar que se ejecutaron alrededor de
+# 40,000 operaciones.
+$ go run atomic-counters.go
+ops: 40200
+
+# A continuación veremos _mutexes_, otra herramienta para administrar estados.
