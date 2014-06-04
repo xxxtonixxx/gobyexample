cdd2842bb2d1ee023c63c187206861841f8baa0b
OscarRyz <oscarryz@gmail.com>
2014-06-04 00:17:26 -0500

    t/tickers/tickers

--- examples/tickers/tickers.sh
+++ examples/tickers/tickers.sh
@@ -1,7 +1,7 @@
-# When we run this program the ticker should tick 3 times
-# before we stop it.
+# Cuando corremos este programa, debe de hacer tic tres
+# veces antes de que lo detengamos.
 $ go run tickers.go
-Tick at 2012-09-23 11:29:56.487625 -0700 PDT
-Tick at 2012-09-23 11:29:56.988063 -0700 PDT
-Tick at 2012-09-23 11:29:57.488076 -0700 PDT
-Ticker stopped
+Tic a las 2014-06-03 23:32:57.180114752 -0500 CDT
+Tic a las 2014-06-03 23:32:57.680578946 -0500 CDT
+Tic a las 2014-06-03 23:32:58.180938733 -0500 CDT
+Ticker detenido
