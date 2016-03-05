dabb919161e257df968c4f8e6862d33283308d58
Toni <tonivj5@Gmail.com>
2016-03-05 17:14:43 +0100

    Correciones

--- examples/range-sobre-canales/range-sobre-canales.sh
+++ examples/range-sobre-canales/range-sobre-canales.sh
@@ -2,6 +2,6 @@ $ go run range-sobre-canales.go
 uno
 dos
 
-# Este ejemplo también mostro como es posible cerrar
+# Este ejemplo también muestra cómo es posible cerrar
 # un canal que no está vacío y aún conservar los
 # valores que no se han recibido.
